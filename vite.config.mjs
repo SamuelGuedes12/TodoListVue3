462c310d2ff97d656acea946a0f1cadf7fc14ad1
samuel guedes <samuelgpb2010@gmail.com>
2025-07-15 12:03:56 -0300

    new config vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -30,13 +30,21 @@ export default defineConfig({
             src: '/web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'maskable',
+            purpose: 'any maskable',
           },
           {
             src: '/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable',
+            purpose: 'any maskable',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/screenshots/screenshot1.png',
+            sizes: '540x720',
+            type: 'image/png',
+            form_factor: 'wide',
           },
         ],
       },
